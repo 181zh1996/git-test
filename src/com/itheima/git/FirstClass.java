88a87632fdc2fdcf9299053710fedc5cb8542add
zhangheng <1215674493@qq.com>
2019-02-17 17:03:01 +0800

    修改FirstClass

--- src/com/itheima/git/FirstClass.java
+++ src/com/itheima/git/FirstClass.java
@@ -4,4 +4,11 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    public void  test(){
+        int i =10;
+        for (int a=0; a<=10;a++){
+            i+=a;
+        }
+    }
 }
